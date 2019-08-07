81cbf995c427cea63fefba5ac08825de2421b692
Ashwin Vidiyala <ashwinvidiyala@gmail.com>
2019-08-06 21:11:46 -0500

    Create FileConverter class

---
+++ file_converter.rb
@@ -0,0 +1,36 @@
+# Create converted file
+class FileConverter
+  attr_reader :headers, :positions, :input_file, :output_file
+
+  def initialize(headers:, positions:, input_file:, output_file:)
+    @headers = headers
+    @positions = positions
+    @input_file = input_file
+    @output_file = output_file
+
+    create_output_file_with_headers
+  end
+
+  def convert
+    CSV.foreach(input_file) do |r|
+      r.each do |row|
+        CSV.open(output_file, 'a') do |csv|
+          data = []
+          positions.each do |pos|
+            data << row[pos]
+          end
+          csv << data
+        end
+      end
+    end
+  end
+
+  private
+
+  def create_output_file_with_headers
+    return if File.exist?(output_file)
+
+    FileUtils.touch(output_file)
+    File.write(output_file, headers, mode: 'w')
+  end
+end
