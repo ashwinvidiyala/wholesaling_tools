ce66b2a247419a4714af4340f9c17eb6a52365f2
Ashwin Vidiyala <ashwinvidiyala@gmail.com>
2019-08-06 20:45:43 -0500

    Add Converter class

--- converter.rb
+++ converter.rb
@@ -2,28 +2,50 @@ require 'csv'
 require 'pry'
 require 'fileutils'
 require_relative 'headers_and_positions'
+require_relative 'output_filename_generator'
 
-filename = ARGV[0]
-output_filename = OutputFilenameGenerator.new(filename)
-
-headers = HeadersAndPositions::ACCOUNT_MASTER[:headers].join(',') + "\n"
-positions = HeadersAndPositions::ACCOUNT_MASTER[:positions]
-
-# Create output file (if needed) with headers
-unless File.exist?(output_filename)
-  FileUtils.touch(output_filename)
-  File.write(output_filename, headers, mode: 'a')
+ARGV.each do |argv|
+  Converter.new(
+    headers: HeadersAndPositions::ACCOUNT_MASTER[:headers].join(',') + "\n",
+    positions: HeadersAndPositions::ACCOUNT_MASTER[:positions],
+    input_file: argv,
+    output_filename: OutputFilenameGenerator.new(filename)
+  ).convert
 end
 
-# Run through input file and save to output file simultaneously
-CSV.foreach(filename) do |r|
-  r.each do |row|
-    CSV.open(output_filename, 'a') do |csv|
-      data = []
-      positions.each do |pos|
-        data << row[pos]
+# Create converted file
+class Converter
+  attr_reader :headers, :positions, :input_file, :output_file
+
+  def initialize(headers:, positions:, input_file:, output_file:)
+    @headers = headers
+    @positions = positions
+    @input_file = input_file
+    @output_file = output_file
+
+    create_output_file_with_headers
+  end
+
+  def convert
+    CSV.foreach(filename) do |r|
+      r.each do |row|
+        CSV.open(output_file, 'a') do |csv|
+          data = []
+          positions.each do |pos|
+            data << row[pos]
+          end
+          csv << data
+        end
       end
-      csv << data
     end
+  end
+
+  private
+
+  def create_output_file_with_headers
+    return if File.exist?(output_file)
+
+    FileUtils.touch(output_file)
+    File.write(output_file, headers, mode: 'a')
   end
 end
