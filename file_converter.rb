5f7dd6d91fc8b9f2d534e479a7c102a48b92995a
Ashwin Vidiyala <ashwinvidiyala@gmail.com>
2019-08-11 23:41:02 -0500

    Comment about using gsub and catching errors
    I've been trying to do these things (so that I don't have to manually
    replace bad characters in vim and also manually delete lines with bad
    UTF coding, but I'm just not able to get it to work)

--- file_converter.rb
+++ file_converter.rb
@@ -14,6 +14,12 @@ class FileConverter
   def convert
     CSV.foreach(input_file) do |r|
       r.each do |row|
+        # row&.tr!('\"', '\'')&.tr!(',', '|')
+        # Do a gsub here to replace " with ' and , with /
+        # Also have a rescue block here to prevent CSV:MalformedError from
+        # breaking shit. Just ignore the current line and go to the next. Or you
+        # can delete it too. It's probably best not to delete the line though.
+        # Use your best judgement.
         CSV.open(output_file, 'a') do |csv|
           data = []
           positions.each do |pos|
