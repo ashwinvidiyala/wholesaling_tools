2c387f13a05ffa4f7d634afa37cf0b830d61ea35
Ashwin Vidiyala <ashwinvidiyala@gmail.com>
2019-08-11 23:43:09 -0500

    Ignore records with an invalid delinquency date in output
    I have defined (for now) an "invalid" delinquency date as one which is
    either in the past (because it's probably too late to help them out at
    that point) or one that's more than 180 days in the future (this I might
    need to give some more thought, but for now it should be fine)

--- file_converter.rb
+++ file_converter.rb
@@ -21,6 +21,8 @@ class FileConverter
         # can delete it too. It's probably best not to delete the line though.
         # Use your best judgement.
         CSV.open(output_file, 'a') do |csv|
+          next if delinquency_date_is_not_valid?(row)
+
           data = []
           positions.each do |pos|
             data << row[pos]
@@ -37,5 +39,49 @@ class FileConverter
     formatted_headers = headers.join(',') + "\n"
     FileUtils.touch(output_file)
     File.write(output_file, formatted_headers, mode: 'w')
+  end
+
+  def positions_range_value_of_header(header)
+    positions[headers.index(header)]
+  end
+
+  def delinquency_date_is_not_valid?(row)
+    date_is_invalid?(delinquency_date(row))
+  end
+
+  def delinquency_date(row)
+    dateify_date(row[positions_range_value_of_header('Delinquency Date')])
+  end
+
+  def convert_to_date_object(date_string)
+    return nil unless date_string
+
+    Date.strptime(date_string, '%m/%d/%Y')
+  rescue ArgumentError => e
+    return nil if e.message.include? 'invalid date'
+  end
+
+  def date_is_in_the_past?(date)
+    return nil unless dateify_date(date)
+
+    dateify_date(date) < Date.today
+  end
+
+  def date_is_too_far_in_the_future?(date)
+    return nil unless dateify_date(date)
+
+    (Date.today + 180) < dateify_date(date)
+  end
+
+  def dateify_date(date)
+    if date.instance_of?(Date)
+      date
+    else
+      convert_to_date_object(date)
+    end
+  end
+
+  def date_is_invalid?(date)
+    date_is_in_the_past?(date) || date_is_too_far_in_the_future?(date)
   end
 end
