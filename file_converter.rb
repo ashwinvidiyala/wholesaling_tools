2ec9301f845fd25bc32c25404d6917953e99948d
Ashwin Vidiyala <ashwinvidiyala@gmail.com>
2019-09-18 20:53:12 -0500

    Skip rows if they meet certain conditions; add little documentation

--- file_converter.rb
+++ file_converter.rb
@@ -1,5 +1,12 @@
-# Create converted file
+# frozen_string_literal: true
+
+# Create converted file. Look at README for more information about the constants
+# used here.
 class FileConverter
+  SPTB_CODES_TO_CONVERT = [1, 18].freeze
+  PRIOR_YEAR_AMOUNT_DUE_MINIMUM = 1000
+  OWNER_NAMES_TO_AVOID = ['texas', 'fort worth', 'tarrant', 'fidelity'].freeze
+  DELINQUENCY_DATE_TO_AVOID = '01/01/9999'
   attr_reader :headers, :positions, :input_file, :output_file
 
   def initialize(headers:, positions:, input_file:, output_file:)
@@ -15,6 +22,7 @@ class FileConverter
     CSV.foreach(input_file) do |r|
       r.each do |row|
         CSV.open(output_file, 'a') do |csv|
+          next if row_to_be_ignored?(row)
 
           data = []
           positions.each do |pos|
@@ -32,5 +40,33 @@ class FileConverter
     formatted_headers = headers.join(',') + "\n"
     FileUtils.touch(output_file)
     File.write(output_file, formatted_headers, mode: 'a')
+  end
+
+  def header_value_in_given_row(row, header)
+    row[positions[headers.index(header)]]
+  end
+
+  def row_to_be_ignored?(row)
+    sptb_code_to_be_ignored?(row) ||
+      delinquency_date_to_be_avoided?(row) ||
+      prior_year_amount_due_is_not_high_enough?(row) ||
+      owner_name_to_be_avoided?(row)
+  end
+
+  def sptb_code_to_be_ignored?(row)
+    !SPTB_CODES_TO_CONVERT.include? header_value_in_given_row(row, 'SPTB Code').to_i
+  end
+
+  def prior_year_amount_due_is_not_high_enough?(row)
+    header_value_in_given_row(row, 'Prior Year Amount Due').to_i < PRIOR_YEAR_AMOUNT_DUE_MINIMUM
+  end
+
+  def owner_name_to_be_avoided?(row)
+    owner_name = header_value_in_given_row(row, 'Owner Name 1').downcase
+    OWNER_NAMES_TO_AVOID.any? { |name| owner_name.include? name }
+  end
+
+  def delinquency_date_to_be_avoided?(row)
+    DELINQUENCY_DATE_TO_AVOID == header_value_in_given_row(row, 'Delinquency Date')
   end
 end
