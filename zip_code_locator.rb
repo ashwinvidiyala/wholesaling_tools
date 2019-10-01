46f1fa4b2fe44d21ce9cb2de8fd0c3bdbb0b76c6
Ashwin Vidiyala <ashwinvidiyala@gmail.com>
2019-10-01 18:00:29 -0500

    Make ZipCodeLocator work, and also add an option to use street address
    The default right now is to use latitude and longitude
    The next step would be to accept user input to see which to use.

--- zip_code_locator.rb
+++ zip_code_locator.rb
@@ -17,24 +17,38 @@ class ZipCodeLocator
   end
 
   def find_zip_codes
-    CSV.open(@input_file, 'r', headers: true, header_converters: :symbol).each do |line|
+    CSV.open(input_file, 'r', headers: true, header_converters: :symbol).each do |line|
       puts "Zip Code for '#{line[:violation_address]}' is #{zip_code(line)}"
+      line << zip_code(line)
+      write_line_to_output_file(line)
     end
   end
 
   private
 
-  def zip_code(line)
-    results = Geocoder.search([line[:latitude], line[:longitude]])
+  def create_output_file_with_headers
+    headers = CSV.table(@input_file).headers.map { |header| header.to_s.capitalize }
+    headers << 'Zip_Code'
+    formatted_headers = headers.join(',') + "\n"
+    FileUtils.touch(output_file)
+    File.write(output_file, formatted_headers, mode: 'w')
+  end
+
+  def zip_code(line, use_street_address: false)
+    results = if use_street_address
+                Geocoder.search("#{line[:violation_address]}, Fort Worth Texas")
+              else
+                Geocoder.search([line[:latitude], line[:longitude]])
+              end
 
     return unless results&.first
 
     results.first.data['address']['postcode'].to_i
   end
 
-  def create_output_file_with_headers
-    formatted_headers = HEADERS.join(',') + "\n"
-    FileUtils.touch(output_file)
-    File.write(output_file, formatted_headers, mode: 'w')
+  def write_line_to_output_file(line)
+    CSV.open(output_file, 'a') do |csv|
+      csv << line
+    end
   end
 end
